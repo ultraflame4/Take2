b80b5fdee4e0388d8da3a7befa8e12dbddf52386
ultraflame4 <ultraflame4@gmail.com>
2020-11-25 22:12:22 +0800

    - cinemachine camera

---
+++ Take2/Assets/Scripts/SetCameraTarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class SetCameraTarget : MonoBehaviour
+{
+    private string playerTag = "Player";
+    public CinemachineVirtualCamera VirtualCam;
+
+    private void Awake()
+    {
+        // TODO : When implementing multiplayer, switch to .FindGameObjectsWithTag and check if is local
+        GameObject player = GameObject.FindGameObjectWithTag(playerTag);
+        VirtualCam.Follow = player.transform;
+    }
+}
